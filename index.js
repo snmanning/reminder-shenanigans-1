2b3a26fb2070a3774eb342e994cd3f9729644d9e
Justin Dennison <justin1dennison@gmail.com>
2018-06-15 18:19:22 -0400

    parsed the command line arguments from the process.argv global available in node

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const fs = require('fs'); //this tells node that we need this module (set of functions and attributes)
-console.log("Welcome to remindme");
 
 const reminderFile = "reminders.txt";
+
+//list command, where all of the reminders are printed
+//add command where we can add a reminder
+const args = process.argv.slice(2);
+const subcommand = args[0];
+if(subcommand === 'list'){
+    console.log('list');
+} else if(subcommand === 'add'){
+    console.log('add');
+} else {
+    console.log('Please check your command syntax');
+}
+process.exit(0);
+
 fs.unlinkSync(reminderFile);
 const reminders = [
     "get some milk", //0
